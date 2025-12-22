d670ebccf757bf81e7509c17c3bcf6f9dfe237c0
anas <elwahabianas7@gmail.com>
2025-12-22 18:07:06 +0100

    update loginRequest API call

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
-
-export const loginRequest = (payload) =>
-  axios.post(`${API_URL}/login`, payload);
+export const loginRequest = (credentials) => {
+  return axios.post(
+    "http://localhost:8080/api/auth/login",
+    credentials
+  );
+};
